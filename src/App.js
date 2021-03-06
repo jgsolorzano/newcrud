014f1b8e9e9a92dc3e0559ef23ec3a36e8c53ab2
Juan <jgsolorzano@gmail.com>
2021-03-06 05:51:15 -0400

    task added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import { isEmpty } from 'lodash'
+import shortid from 'shortid'
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+
+  const addTask=(e)=>{
+    //evitar que recargue la pagina por el submit
+    e.preventDefault()
+    if(isEmpty(task)){
+      console.log("Task empty")
+      return
+    }
+    //console.log("Ok")
+    const newTask = {
+      id:shortid.generate(),
+      name: task
+    }
+    setTasks([...tasks, newTask])
+    setTask("")
+  }
+  //prueba de actualizacion y otra mas porque no funciona a la primera
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5"> 
+      <h1>Tareas</h1>
+      <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+            {
+              tasks.map((task)=>(
+                <li className="list-group-item" key={task.id}>
+                  <span className="lead">{task.name}</span>
+                  <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                  <button className="btn btn-warning btn-sm float-right">Editar</button>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+          <form onSubmit={addTask}>
+            <input 
+              type="text"
+              className="form-control mb-2"
+              placeholder="Ingrese la tarea..."
+              onChange={(text) => setTask(text.target.value)}
+              value={task}
+            />
+            <button 
+              className="btn btn-dark btn-block"
+              type="submit"
+            >Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
